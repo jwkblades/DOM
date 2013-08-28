a48224d4cd0751e6ca4d0a0e901d4388bfb6b8e4
James Blades <jwkblades@gmail.com>
2013-08-27 21:56:49 -0600

    First commit. Hopefully things are in the same place I had them with the original calendar example.

---
+++ js/dom.js
@@ -0,0 +1,73 @@
+var DOM = {
+	"version": 0.1,
+	"name": "DOM",
+	"description": "A simple DOM library",
+	"author": "James Blades",
+	"contributors": [
+		"James Blades"
+	],
+	init = function(){
+		var e = Element;
+		if(!e){
+			throw "Error - Unsupported browser";
+		}
+		e.prototype.after = function(ele){
+			if(ele){
+				this.parent().insertBefore(ele, this.next());
+			}
+			return this.next();
+		};
+		e.prototype.append = function(ele){
+			this.appendChild(ele);
+		};
+		e.prototype.before = function(ele){
+			if(ele){
+				this.parent().insertBefore(ele, this);
+			}
+			return this.previous();
+		};
+		e.prototype.clone = function(){
+			return this.cloneNode(true);
+		};
+		e.prototype.create = DOM.create;
+		e.prototype.delete = function(){
+			this.parent().removeChild(this);
+		};
+		e.prototype.first = function(){
+			var current = this.firstChild;
+			while(String(current.appendData).substr(0, 9) === "function"){
+				current = this.previous();
+			}
+			return current;
+		};
+		e.prototype.last = function(){
+			var current = this.lastChild;
+			while(String(current.appendData).substr(0, 9) === "function"){
+				current = this.previous();
+			}
+			return current;
+		};
+		e.prototype.next = function(){
+			return this.nextSibling;
+		};
+		e.prototype.parent = function(){
+			return this.parentNode;
+		};
+		e.prototype.prepend = function(ele){
+			this.insertBefore(ele, this.first());
+		};
+		e.prototype.previous = function(){
+			return this.previousSibling;
+		};
+		e.prototype.remove = function(ele){
+			ele.delete();
+		};
+		e.prototype.text = DOM.create;
+	},
+	create: function(tag){
+		return document.createElement(ele);
+	},
+	text: function(str){
+		return document.createTextNode(str);
+	}
+};
